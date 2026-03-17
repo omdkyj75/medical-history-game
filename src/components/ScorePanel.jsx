c671885719cbfe8c7351be5b49a6477c063c5253
omdkyj75 <omdkyj75@gmail.com>
2026-03-17 11:30:04 +0900

    Update ScorePanel.jsx

--- src/components/ScorePanel.jsx
+++ src/components/ScorePanel.jsx
@@ -1,1 +1,13 @@
-
+export default function ScorePanel({ scores, labels }) {
+  return (
+    <section className="score-panel">
+      <h3>현재 성장 상태</h3>
+      <ul>
+        <li>{labels.academicReputation}: {scores.academicReputation}</li>
+        <li>{labels.clinicalTrust}: {scores.clinicalTrust}</li>
+        <li>{labels.textUnderstanding}: {scores.textUnderstanding}</li>
+        <li>{labels.publicFavor}: {scores.publicFavor}</li>
+      </ul>
+    </section>
+  );
+}
