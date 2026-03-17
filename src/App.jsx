c9e4381b0a7d090a943e2002174acc420dfc0c18
omdkyj75 <omdkyj75@gmail.com>
2026-03-17 11:17:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import { useGameState } from "./hooks/useGameState";
+import StartPage from "./components/StartPage";
+import HowToPlayPage from "./components/HowToPlayPage";
+import StagePage from "./components/StagePage";
+import FinalResultPage from "./components/FinalResultPage";
+import ResultModal from "./components/ResultModal";
 
+export default function App() {
+  const game = useGameState();
+
+  return (
+    <div className="app-shell">
+      {game.screen === "start" && <StartPage game={game} />}
+      {game.screen === "howToPlay" && <HowToPlayPage game={game} />}
+      {game.screen === "stage" && <StagePage game={game} />}
+      {game.screen === "final" && <FinalResultPage game={game} />}
+
+      {game.isResultModalOpen && (
+        <ResultModal
+          result={game.selectedChoiceResult}
+          onNext={game.goToNextStage}
+          onViewProgress={game.goToProgress}
+        />
+      )}
+    </div>
+  );
+}
